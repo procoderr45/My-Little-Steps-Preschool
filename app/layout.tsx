bde4f7ae10d2e2b103cc7608b72c5aa8aaeecf7e
Om Takbhate <omtakbhate007@gmail.com>
2026-02-18 01:18:14 +0530

    feat: SEO google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,23 @@ const playfair = Playfair_Display({
   variable: "--font-playfair",
 })
 
+
+export const metadata = {
+  title: "Best Preschool in Mumbai | My Little Steps Preschool, Ambarnath",
+  description:
+    "Discover our philosophy, core values, and nurturing approach at My Little Steps Preschool in Ambarnath.",
+  keywords: [
+    "Best preschool in Ambarnath",
+    "Luxury preschool Mumbai",
+    "Montessori preschool Ambarnath",
+    "CBSE preschool near me"
+  ],
+
+  verification: {
+    google: "4AvwA2rd-SSD7gUUN5Hqq3n7X9e1IjPPh0FWxCysGz4",
+  },
+}
+
 const inter = Inter({
   subsets: ["latin"],
   weight: ["300", "400", "500"],
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import Navbar from "@/features/app/Navbar";
 import AboutPage from "@/features/landing/AboutUs";
 import HeroSection from "@/features/landing/HeroComponents";
-import Image from "next/image";
-
-export const metadata = {
-  title: "Best Preschool in Mumbai | My Little Steps Preschool, Ambarnath",
-  description:
-    "Discover our philosophy, core values, and nurturing approach at My Little Steps Preschool in Ambarnath.",
-  keywords: [
-    "Best preschool in Ambarnath",
-    "Luxury preschool Mumbai",
-    "Montessori preschool Ambarnath",
-    "CBSE preschool near me"
-  ],
-
-  verification: {
-    google: "4AvwA2rd-SSD7gUUN5Hqq3n7X9e1IjPPh0FWxCysGz4",
-  },
-}
 
 
 export default function Home() {
